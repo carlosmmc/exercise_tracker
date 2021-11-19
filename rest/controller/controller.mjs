49cbd1b449e34a346ef63e002931e00d47c7f061
Carlos Martinez-Cabello <carlosmmc123@gmail.com>
2021-11-18 18:25:02 -0600

    full rest CRUD

--- rest/controller/controller.mjs
+++ rest/controller/controller.mjs
@@ -1,41 +1,38 @@
 // main controller code which implements CRUD functionality
 
 import Exercise from '../model/Exercise.mjs'
-import { updateHelper, generateFilter } from './helper.mjs'
 
 const create = async (req, res, next) => {
     const newExercise = new Exercise(req.body)
     const resp = await newExercise.save()
-    res.status(200).send(resp)
+
+    res.setHeader('content-type', 'application/json');
+    res.status(201).send(resp)
 }
 
 const retrieve = async (req, res, next) => {
-    const filter = generateFilter(req.query)
-    const query = Exercise.find()
+    const result = await Exercise.find({})
 
-    if (filter.length > 0) {
-        query.and(filter)
-    }
-
-    const result = await query.exec()
-    res.send(result)
+    res.setHeader('content-type', 'application/json');
+    res.status(200).send(result)
 }
 
 const update = async (req, res, next) => {
-    const { filter, update } = updateHelper(req.query)
-    const result = await Exercise.findOneAndUpdate(filter, update)
+    const { id } = req.params
+    const updatedValues = req.body
+    const result = await Exercise.findByIdAndUpdate(id, updatedValues, { new: true })
 
-    if (result !== null) {
-        res.send({ "modifiedCount": 1 })
-    } else {
-        res.send({ "Error": "Not found" })
-    }
+    res.setHeader('content-type', 'application/json');
+    res.status(200).send(result)
 }
 
 
 const remove = async (req, res, next) => {
-    const result = await Exercise.deleteMany(req.query)
-    res.send(result)
+    const { id } = req.params
+    const result = await Exercise.findByIdAndDelete(id)
+
+
+    res.status(204).send(result)
 }
 
 export { create as createExercise, update as updateExercise, retrieve as retrieveExercise, remove as deleteExercise }
