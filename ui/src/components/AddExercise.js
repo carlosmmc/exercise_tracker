4d225e2719aeb57936b98747ca292cbc4da45a62
Carlos Martinez-Cabello <carlosmmc123@gmail.com>
2021-11-19 21:31:31 -0600

    create functionality in ui

--- ui/src/components/AddExercise.js
+++ ui/src/components/AddExercise.js
@@ -1,7 +1,31 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function AddExercise() {
-    const [zip, setZip] = useState('')
+    const [name, setName] = useState('')
+    const [reps, setReps] = useState('')
+    const [weight, setWeight] = useState('')
+    const [date, setDate] = useState('')
+    const history = useHistory()
+
+    const submitEntry = async () => {
+        const unit = document.getElementById("unit").value;
+        const entry = { name, reps, weight, 'unit': unit, date }
+        const response = await fetch('/exercises', {
+            method: 'POST',
+            body: JSON.stringify(entry),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+
+        if (response.status === 201) {
+            alert('Successfully added new exercise!')
+        } else {
+            alert(`Failed to add exercise, the status code of the request was ${response.status}.`)
+        }
+        history.push('/')
+    }
 
     return (
         <form>
@@ -10,13 +34,13 @@ function AddExercise() {
             <fieldset>
 
                 <label for="name">Name</label>
-                <input type="text" id="name" value={zip} onChange={e => setZip(e.target.value)} />
+                <input type="text" id="name" value={name} onChange={e => setName(e.target.value)} />
 
                 <label for="reps"># Reps</label>
-                <input type="number" id="reps" value={zip} onChange={e => setZip(e.target.value)} />
+                <input type="number" id="reps" value={reps} onChange={e => setReps(e.target.value)} />
 
                 <label for="weight">Weight</label>
-                <input type="number" id="weight" value={zip} onChange={e => setZip(e.target.value)} />
+                <input type="number" id="weight" value={weight} onChange={e => setWeight(e.target.value)} />
 
                 <label for="unit">Unit</label>
                 <select name="unit" id="unit">
@@ -25,12 +49,12 @@ function AddExercise() {
                 </select>
 
                 <label for="date">Date</label>
-                <input type="text" id="date" value={zip} onChange={e => setZip(e.target.value)} />
+                <input type="text" id="date" value={date} onChange={e => setDate(e.target.value)} />
 
             </fieldset>
 
             <button onClick={e => {
-                alert(`Exercise Saved`)
+                submitEntry()
                 e.preventDefault()
             }}>Submit</button>
 
